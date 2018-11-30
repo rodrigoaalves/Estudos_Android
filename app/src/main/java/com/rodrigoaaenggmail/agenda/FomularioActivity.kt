192c8ba7de288a2e59313d7c3b0a2902649514bc
Rodrigo A Alves <rodrigo.alves@setis.com.br>
2018-11-30 15:36:20 -0200

    Primeira Versao

---
+++ app/src/main/java/com/rodrigoaaenggmail/agenda/FomularioActivity.kt
@@ -0,0 +1,30 @@
+package com.rodrigoaaenggmail.agenda
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_fomulario.*
+
+class FomularioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fomulario)
+        setSupportActionBar(toolbar)
+
+        val ButtonSave = findViewById<Button>(R.id.formulario_btSalvar)
+        ButtonSave.setOnClickListener {view -> val intent =
+            Toast.makeText (this@FomularioActivity, "Botao clicado", Toast.LENGTH_SHORT).show()
+        }
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/rodrigoaaenggmail/agenda/ListaAlunosActivit.kt
@@ -0,0 +1,31 @@
+package com.rodrigoaaenggmail.agenda
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+import kotlinx.android.synthetic.main.activity_lista_alunos.*
+
+class ListaAlunosActivit : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_alunos)
+        setSupportActionBar(toolbar)
+
+        val alunos = arrayOf("Rodrigo", "Thiago", "Maria")
+        val listaAlunos = findViewById<ListView>(R.id.lista_alunos)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos)
+
+        listaAlunos.adapter = adapter
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+
+}

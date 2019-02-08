943738bf7fda5d59fbf8116cc5ee644a0e4e216a
Rodrigo A Alves <rodrigo.alves@setis.com.br>
2019-02-08 14:03:39 -0200

    [Aula3] Atividade 3 - incrementando a foto no na view

--- app/src/main/java/com/rodrigoaaenggmail/agenda/Adapter/AlunosAdapter.java
+++ app/src/main/java/com/rodrigoaaenggmail/agenda/Adapter/AlunosAdapter.java
@@ -1,11 +1,16 @@
 package com.rodrigoaaenggmail.agenda.Adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.rodrigoaaenggmail.agenda.ListaAlunosActivit;
+import com.rodrigoaaenggmail.agenda.R;
 import com.rodrigoaaenggmail.agenda.modelo.Aluno;
 
 import java.util.List;
@@ -35,10 +40,32 @@ public class AlunosAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        TextView view = new TextView(context);
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
         Aluno aluno = alunos.get(position);
-        view.setText (aluno.toString());
+        View view = convertView;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        if (view == null) {
+            view = inflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        View campoNome = (TextView) view.findViewById (R.id.item_nome);
+        ((TextView) campoNome).setText (aluno.getNome());
+
+        View campoTelefone = (TextView) view.findViewById(R.id.item_telefone);
+        ((TextView) campoTelefone).setText (aluno.getTelefone());
+
+        View campoFoto = (ImageView) view.findViewById(R.id.item_foto);
+        String caminhoFoto = aluno.getCaminhoFoto();
+        if (caminhoFoto != null) {
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 100, 100, true);
+            ((ImageView) campoFoto).setImageBitmap(bitmapReduzido);
+            campoFoto.setTag(caminhoFoto);
+            ((ImageView) campoFoto).setScaleType(ImageView.ScaleType.FIT_XY);
+        }
+
+
         return view;
     }
 }
